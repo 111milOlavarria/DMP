8da6939957b0c57b8403d25f2530310e582a84ac
111milOlavarria <GroupFour.DMP@gmail.com>
2019-02-08 22:34:33 -0300

    Initial commit

---
+++ src/clases/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import Swing.InterfazUsuario;
+
+/**
+ *
+ * @author Nico
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        InterfazUsuario reg = new InterfazUsuario();
+        reg.setTitle("DMP");
+        reg.setLocationRelativeTo(null);
+        reg.setVisible(true);
+        
+    }
+}

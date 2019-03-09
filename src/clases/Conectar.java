8b057b75df0bd135723f2319b43604e39e44483b
Jose <mcpsoft2000@yahoo.com.ar>
2019-03-08 23:09:23 -0300

    Version 190308-2

--- src/clases/Conectar.java
+++ src/clases/Conectar.java
@@ -1,40 +1,23 @@
-
 package clases;
 
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-
 public class Conectar {
+
     Connection conect = null;
-    public Connection conexion()
-    {
+
+    public Connection conexion() {
         try {
-            //Class.forName("com.mysql.cj.jdbc.Driver");
 
-            //conect = DriverManager.getConnection("jdbc:mysql://localhost:3306/personas","root","root1234");
+            conect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dmp?serverTimezone=America/Argentina/Buenos_Aires", "root", "21939786");
 
-//                        try {
-//                Class.forName("com.mysql.cj.jdbc.Driver");
-//            } catch (ClassNotFoundException ex) {
-//                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-            
-            //conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd111mil?serverTimezone=America/Argentina/Buenos_Aires", usuario, contraseña);
-       
-            conect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dmp?serverTimezone=America/Argentina/Buenos_Aires", "root", "21939786");            
-              
-        
-        } //catch (ClassNotFoundException | SQLException e) {
-        catch (SQLException e) {
-        
-            JOptionPane.showMessageDialog(null, e.getMessage(), "\nAdvertencia", JOptionPane.WARNING_MESSAGE);                
-        
+        } catch (SQLException e) {
+
+            JOptionPane.showMessageDialog(null, e.getMessage(), "\nAdvertencia", JOptionPane.WARNING_MESSAGE);
+
         }
 
         return conect;
-    }     
+    }
 }
-     
-
-
--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clases;
 
 import Swing.InterfazUsuario;
 
-/**
- *
- * @author Nico
- */
 public class Principal {
     
     public static void main(String[] args) {
